959257c565c2069ad243d376faca82ab65e2a9d9
Joshua Rodriguez <joshua.david.rodriguez.medrano@gmail.com>
2022-11-24 02:03:34 -0600

    Show favorites on Home

--- src/home.js
+++ src/home.js
@@ -1,10 +1,31 @@
 import { Container, Typography } from "@mui/material";
+import { useFavorites } from "./hooks/useFavorites";
 
 const Home = () => {
+  const { favorites } = useFavorites();
+
+  const allFavorites = Object.values(favorites)?.reduce((a, b) => a.concat(b));
+
   return (
     <Container>
       <Typography variant="h1" sx={{ textAlign: "center" }}>
         Home page
+      </Typography>
+
+      <Typography variant="h5" sx={{ textAlign: "center" }}>
+        {allFavorites?.length} Favorites
+      </Typography>
+
+      <Typography variant="h5" sx={{ textAlign: "center" }}>
+        Continents: {favorites?.continents?.join(", ")}
+      </Typography>
+
+      <Typography variant="h5" sx={{ textAlign: "center" }}>
+        Countries: {favorites?.countries?.join(", ")}
+      </Typography>
+
+      <Typography variant="h5" sx={{ textAlign: "center" }}>
+        Languages: {favorites?.languages?.join(", ")}
       </Typography>
     </Container>
   );
